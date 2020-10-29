13807b8760fb6e22cff00515a2a41140f3ea1838
Glendon Thompson <glendonthompson1@gmail.com>
2020-10-29 08:24:32 -0500

    updating

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import multiprocessing as mp
 
-from hypercorn.config import Config
-
-config = Config()
-config.bind = ["localhost:8000"]
-config.workers = 2
-
 if __name__ == "__main__":
     mp.freeze_support()
     mp.set_start_method('spawn')
